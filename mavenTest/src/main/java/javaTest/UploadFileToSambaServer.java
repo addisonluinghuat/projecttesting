3b96f0146fd255b64e11a20611e48697b420f066
addison.lu <addison.lu@sicmsb.com>
2019-10-29 14:19:19 +0800

    maven test

---
+++ mavenTest/src/main/java/javaTest/UploadFileToSambaServer.java
@@ -0,0 +1,41 @@
+package javaTest;
+
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileOutputStream;
+
+public class UploadFileToSambaServer {
+	static final String USER_NAME = "pi";
+	static final String PASSWORD = "raspberry";
+	// e.g. Assuming your network folder is: \my.myserver.netsharedpublicphotos
+	static final String NETWORK_FOLDER = "smb://raspberrypi.local/share/";
+
+	public static void main(String args[]) {
+		String fileContent = "This is a test file";
+
+		new UploadFileToSambaServer().copyFiles(fileContent, "testFile.text");
+	}
+
+	public boolean copyFiles(String fileContent, String fileName) {
+		boolean successful = false;
+		try {
+			String user = USER_NAME + ":" + PASSWORD;
+			System.out.println("User: " + user);
+
+			NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(user);
+			String path = NETWORK_FOLDER + fileName;
+			System.out.println("Path: " + path);
+
+			SmbFile sFile = new SmbFile(path, auth);
+			SmbFileOutputStream sfos = new SmbFileOutputStream(sFile);
+			sfos.write(fileContent.getBytes());
+
+			successful = true;
+			System.out.println("Successful" + successful);
+		} catch (Exception e) {
+			successful = false;
+			e.printStackTrace();
+		}
+		return successful;
+	}
+}
